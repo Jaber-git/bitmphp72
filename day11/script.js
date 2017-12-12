efae095c64ab5a2945542601b84d657efbdc6bd3
monirul89 <monirul89@gmail.com>
2017-12-13 00:04:05 +0600

    update

--- day11/script.js
+++ day11/script.js
@@ -1,11 +1,47 @@
 
 var btnElement = document.getElementById('btnPlus');
 btnElement.onclick = function () {
-    var firstNameValue = document.getElementById('firstName').value;
-    var lastNameValue = document.getElementById('lastName').value;
-    var fullNameValue = firstNameValue +' '+ lastNameValue;
+    var firstValue = document.getElementById('firstNumber').value;
+    var secondValue = document.getElementById('secondNumber').value;
+    var fullValue = parseInt(firstValue) + parseInt(secondValue);
 
-    document.getElementById('fullName').value=fullNameValue;
+    document.getElementById('fullNumber').value=fullValue;
+
+}
+var btnElement = document.getElementById('btnMinus');
+btnElement.onclick = function () {
+    var firstValue = document.getElementById('firstNumber').value;
+    var secondValue = document.getElementById('secondNumber').value;
+    var fullValue = parseInt(firstValue) - parseInt(secondValue);
+
+    document.getElementById('fullNumber').value=fullValue;
+
+}
+var btnElement = document.getElementById('btnMulti');
+btnElement.onclick = function () {
+    var firstValue = document.getElementById('firstNumber').value;
+    var secondValue = document.getElementById('secondNumber').value;
+    var fullValue = parseInt(firstValue) * parseInt(secondValue);
+
+    document.getElementById('fullNumber').value=fullValue;
+
+}
+var btnElement = document.getElementById('btnDiv');
+btnElement.onclick = function () {
+    var firstValue = document.getElementById('firstNumber').value;
+    var secondValue = document.getElementById('secondNumber').value;
+    var fullValue = parseInt(firstValue) / parseInt(secondValue);
+
+    document.getElementById('fullNumber').value=fullValue;
+
+}
+var btnElement = document.getElementById('btnMod');
+btnElement.onclick = function () {
+    var firstValue = document.getElementById('firstNumber').value;
+    var secondValue = document.getElementById('secondNumber').value;
+    var fullValue = parseInt(firstValue) % parseInt(secondValue);
+
+    document.getElementById('fullNumber').value=fullValue;
 
 }
 
