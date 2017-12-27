859bb5f8692cb3a3d7dd2d09e54bab5f6f09c05a
monirul89 <monirul89@gmail.com>
2017-12-27 12:15:59 +0600

    update

--- day15/project.php
+++ day15/project.php
@@ -172,7 +172,69 @@ $y=20;
                         //FOR, WHILE, DO WHILE, FOREACH
 
 
+		echo '<h3>switch statement</h3>';
+        echo "<br/>";
+        $number = 50;
 
+        switch ($number) {
+            case 20:
+                echo "Your card number is ".$number;
+                break;
+            case 30:
+                echo "Your card number is ".$number;
+                break;
+            case 50:
+                echo "Your card number is ".$number;
+                break;
+            default:
+                echo "You don't have card number ";
+        }
+		//Your card number is 50
+		
+		echo '<hr/>';
+        echo '<h3>Statement (Repeater)</h3>';
+		echo "<br/>";
+		echo "<br/>";
+		echo "<br/>";
+        echo '<h3>Statement (Repeated-FOR Loop)</h3>';
+		
+        echo "<br/>";
+		$x=10;
+
+        for ($x=0; $x<=10; $x++){
+            echo $x." ";
+        } //0 1 2 3 4 5 6 7 8 9 10
+		 echo "<br />";
+		 
+		 echo "<br />";
+		 
+		 for ($x; $x>=0; $x--){
+            echo $x."<br />";
+        }
+		
+		
+        echo "<br/>";echo "<br/>";
+		echo '<h3>Statement (WHILE)</h3>';
+        echo "<br/>";
+		
+		
+        $x= 15;
+
+        while ($x <= 30){
+            echo $x." ";
+            $x++; 
+        }
+		
+        
+		echo '<h3>Statement (DO WHILE)</h3>';
+        echo "<br/>";
+		
+		
+        do {
+            echo $x." ";
+            $x--;
+        } while ($x>=15);
+		echo "<br/>";echo "<br/>";
 ?>
 
 
